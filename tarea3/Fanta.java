62a656fca4ad79540f94f4906aaee2259a985b3b
Bianca <bseminario2023@udec.cl>
2023-11-01 22:26:08 -0500

    documentacion de la clase Fanta

--- tarea3/Fanta.java
+++ tarea3/Fanta.java
@@ -1,11 +1,21 @@
 package tarea3;
-
+/**
+ * clase Fanta que es subclase de Bebida para representar el tipo especifico de bebida
+ */
 class Fanta extends Bebida {
-
+    /**
+     *
+     * @param serie El número de serie de la Fanta.
+     * @param precioProducto El precio de la Fanta, se representa por un objeto PrecioProducto de tipo FANTA.
+     *
+     */
     public Fanta(int serie, PrecioProducto precioProducto) {
         super(serie, precioProducto.FANTA);
     }
-
+    /**
+     * Metodo para simular la accion de ingerir
+     * @return devuelve string para señalar que es bebiendo
+     */
     public String ingerir(){
         return "Fanta";
     }
