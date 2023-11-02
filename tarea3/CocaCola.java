8df0ded86698ff43345cdb799c483bcfc42d7880
Bianca <bseminario2023@udec.cl>
2023-11-01 22:23:25 -0500

    documentacion de la clase cocacola

--- tarea3/CocaCola.java
+++ tarea3/CocaCola.java
@@ -1,11 +1,23 @@
 package tarea3;
 
+/**
+ * clase CocaCola que es subclase de Bebida para representar el tipo especifico de bebida
+ */
 class CocaCola extends Bebida {
-
+    /**
+     *
+     * @param serie El número de serie de la CocaCola.
+     * @param precioProducto El precio de la CocaCola, se representa por un objeto PrecioProducto de tipo COCA.
+     *
+     */
     public CocaCola(int serie, PrecioProducto precioProducto) {
         super(serie, precioProducto.COCA);
     }
 
+    /**
+     * Metodo para simular la accion de ingerir
+     * @return devuelve string para señalar que es bebiendo
+     */
     public String ingerir(){
         return "CocaCola";
     }
