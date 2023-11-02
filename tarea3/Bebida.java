97c5f4b06a130e572a881ad5fa46b9dade841457
Bianca <bseminario2023@udec.cl>
2023-11-01 22:18:42 -0500

    documentacion de la clase bebida

--- tarea3/Bebida.java
+++ tarea3/Bebida.java
@@ -1,6 +1,14 @@
 package tarea3;
- abstract class Bebida extends Producto {
 
+/**
+ *  * La clase abstracta Bebida que extiende de la clase Producto, representa un tipo de producto que es una bebida
+ */
+ abstract class Bebida extends Producto {
+     /**
+      *
+      * @param serie el numero de serie de la bebida
+      * @param precioProducto el precio de la bebida representado por un objeto precioProducto
+      */
     public Bebida(int serie,PrecioProducto precioProducto) {
         super(serie,precioProducto);
     }
