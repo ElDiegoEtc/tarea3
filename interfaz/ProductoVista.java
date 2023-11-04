55369d59a5d644e7c674930ff3c6bc329de5212f
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-11-04 18:48:23 -0300

    Creacion clase ProductoVista

--- interfaz/ProductoVista.java
+++ interfaz/ProductoVista.java
@@ -1,2 +1,18 @@
-package interfaz;public class ProductoVista {
+package interfaz;
+
+import java.awt.*;
+
+public class ProductoVista {
+    private int cordx,cordy,width,heigth;
+    public ProductoVista(int cordx, int cordy, int width, int height){
+        this.cordx = cordx;
+        this.cordy = cordy;
+        this.width = width;
+        this.heigth = height;
+    }
+
+    @Override
+    protected void paint(Graphics g){
+
+    }
 }
