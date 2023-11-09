fadb868b25bf63d104617aa95ce0c1a4e9cb5b2a
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-11-08 22:47:11 -0300

    Planteamiento de logica para creacion de depositos en panel expendedor (comentarios en codigo)

--- interfaz/DepositosVista.java
+++ interfaz/DepositosVista.java
@@ -1,12 +1,14 @@
 package interfaz;
 
+import javax.swing.*;
 import java.awt.*;
 
-public class DepositosVista {
+public class DepositosVista<T> {
     public DepositosVista(){
 
     }
     public void paint(Graphics g){
+
 
     }
 }
