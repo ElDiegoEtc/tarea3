1c72acd153bdf1d4cfa0d37dbd4421939a08261b
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-11-09 10:52:03 -0300

    Creacion de logica de clase DepositosVista como Jpanel

--- interfaz/DepositosVista.java
+++ interfaz/DepositosVista.java
@@ -2,13 +2,22 @@ package interfaz;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class DepositosVista<T> {
-    public DepositosVista(){
+public class DepositosVista<T> extends JPanel{
+    private JList<T> objetos;
+    private DefaultListModel<T> modelo;
 
+    public DepositosVista(ArrayList<T> deposito){
+        objetos = new JList<T>();
+        modelo = new DefaultListModel<>();
+        for (T item : deposito) {
+            modelo.addElement(item);
+        }
+        add(objetos);
     }
-    public void paint(Graphics g){
-
-
+    public void paintComponent(Graphics g){
+        super.paint(g);
+        g.fillRect(40, 50, 30, 30);
     }
 }
