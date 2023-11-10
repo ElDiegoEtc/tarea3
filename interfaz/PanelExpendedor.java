dfc268789ea65846dfccb41c6b5983001aaa859b
Bianca <bseminario2023@udec.cl>
2023-11-09 22:09:58 -0500

    actualizacion de la interfaz expendedor

--- interfaz/PanelExpendedor.java
+++ interfaz/PanelExpendedor.java
@@ -1,82 +1,148 @@
 package interfaz;
-import tarea3.DepositoGenerico;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-class PanelExpendedor extends JPanel {
-    private JTextField cantidadMonedas; // Campo para ingresar cantidad de monedas
-
+public class PanelExpendedor extends JPanel {
+    private JTextField cantidadMonedas;
     private JButton[] buttons;
     private int coinsInserted;
     private int productDeposit;
-    private Button buttonComprar;
-    private tarea3.DepositoGenerico<tarea3.Producto> DepositoProductos;
+    private JButton buttonComprar;
     private Label labelVuelto;
+    private tarea3.Expendedor expendedor;
+    private tarea3.Comprador comprador;
+    private int eleccionProducto = -1;
+    private tarea3.Moneda moneda;
 
     public PanelExpendedor() {
+        expendedor = new tarea3.Expendedor(10);
 
-        // Creamos los controles
-        buttonComprar = new Button("Comprar");
-        DepositoProductos = new DepositoGenerico<>();
+        buttonComprar = new JButton("Comprar");
         labelVuelto = new Label("Vuelto:");
         buttons = new JButton[6];
         coinsInserted = 0;
         productDeposit = 0;
-
-       /* Ejecutar expendedor con 10 productos de cada deposito de producto
-                crear interfaz de cada deposito de acuerdo a deposito vista
-
-
-        if(Se hace click en panel expendedor){
-            if(deposito 1 vacio){
-                rellenar;
-            }
-            if(deposito 2 vacio){
-                rellenar;
-            }
-            ....
-        }*/
-
+/**
+ *
+ */
         for (int i = 0; i < 6; i++) {
             buttons[i] = new JButton(String.valueOf(i + 1));
         }
 
-        setLayout(null); // Establecer un diseño nulo para posicionar los componentes manualmente
+        setLayout(null);
         for (int i = 0; i < 6; i++) {
             int row = i < 3 ? 0 : 1;
             int col = i % 3;
-            buttons[i].setBounds(50 + (col * 50), 50 + (row * 50), 50, 50); // Ajuste de la posición de los botones
+            buttons[i].setBounds(50 + (col * 50), 50 + (row * 50), 50, 50);
             add(buttons[i]);
         }
+
         JButton InsertarMoneda = new JButton("Insertar Moneda");
-        InsertarMoneda.setBounds(50, 370, 150, 30); // Posicionamiento del botón "Insertar Moneda"
+        InsertarMoneda.setBounds(50, 370, 150, 30);
         add(InsertarMoneda);
         JLabel titleLabel = new JLabel("Seleccione Producto", SwingConstants.CENTER);
-        titleLabel.setBounds(50, 150, 150, 30); // Posicionamiento del título "Seleccione Producto"
+        titleLabel.setBounds(50, 150, 150, 30);
         add(titleLabel);
-        buttonComprar.setBounds(150, 200, 150, 30);
+        buttonComprar.setBounds(50, 200, 150, 30);
         add(buttonComprar);
-        labelVuelto.setBounds(190, 400, 150, 30);
+        labelVuelto.setBounds(50, 250, 150, 30);
         add(labelVuelto);
         cantidadMonedas = new JTextField();
-        cantidadMonedas.setBounds(50, 340, 150, 30); // Posicionamiento del campo para ingresar la cantidad de monedas
+        cantidadMonedas.setBounds(50, 340, 150, 30);
         add(cantidadMonedas);
+/**
+ *
+ */
+        InsertarMoneda.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int valorMoneda = Integer.parseInt(cantidadMonedas.getText());
+                moneda = obtenerMonedaConValor(valorMoneda); // Llamada al método
+            }
+        });
+        buttonComprar.addActionListener(new ActionListener() {
+            /**
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (moneda != null && eleccionProducto != -1 && expendedor != null) {
+                    try {
+                        // Intenta crear un comprador con los datos recopilados
+                        comprador = new tarea3.Comprador(moneda, eleccionProducto, expendedor);
+
+                        // Actualizar la interfaz con la información de la compra
+                        labelVuelto.setText("Producto comprado: " + comprador.queBebiste() + ", Vuelto: " + comprador.cuantoVuelto());
+                    } catch (tarea3.NoHayProductoException | tarea3.PagoInsuficienteException | tarea3.PagoIncorrectoException ex) {
+                        // Manejar las excepciones específicas aquí
+                        // Por ejemplo, mostrar un mensaje de error en la interfaz
+                        labelVuelto.setText("Error: " + ex.getMessage());
+                    }
+                } else {
+                    // Manejar si los datos no están completos o válidos
+                    labelVuelto.setText("Por favor, complete todos los datos antes de comprar.");
+                }
+            }
+        });
+
+        // ActionListener para los botones de selección de producto
+        for (int i = 0; i < 6; i++) {
+            buttons[i].addActionListener(new ActionListener() {
+                /**
+                 *
+                 * @param e the event to be processed
+                 */
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Obtener el texto del botón presionado
+                    String buttonText = ((JButton) e.getSource()).getText();
+                    eleccionProducto = Integer.parseInt(buttonText); // Asignar la elección del producto
+                }
+            });
+
+        }
 
     }
 
+    /**
+     *
+     * @param valor
+     * @return
+     */
+    private tarea3.Moneda obtenerMonedaConValor(int valor) {
+        tarea3.Moneda moneda;
+
+        switch (valor) {
+            case 100:
+                moneda = new tarea3.Moneda100();
+                break;
+            case 500:
+                moneda = new tarea3.Moneda500();
+                break;
+            case 1000:
+                moneda = new tarea3.Moneda1000();
+                break;
+            case 1500:
+                moneda = new tarea3.Moneda1500();
+                break;
+            // Agregar más casos según las monedas disponibles
+            default:
+                moneda = null; // Si no se reconoce el valor, retorna null
+                break;
+        }
+
+        return moneda;
+    }
+
+    /**
+     * 
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
-
         super.paintComponent(g);
         // Dibujar el rectángulo que encierra todos los elementos del expendedor
         g.setColor(Color.BLACK);
@@ -86,11 +152,6 @@ class PanelExpendedor extends JPanel {
         for (JButton button : buttons) {
             button.setBackground(Color.PINK);
         }
-        // Dibujar la sección de monedas
-        g.setColor(Color.BLACK);
-        g.fillRect(50, 300, 150, 50);
-        g.setColor(Color.BLACK);
-        g.drawRect(50, 300, 150, 50);
 
         // Dibujar la sección de depósito de productos
         g.setColor(Color.LIGHT_GRAY);
@@ -103,6 +164,8 @@ class PanelExpendedor extends JPanel {
         g.fillRect(300, 50, 200, 470);
         g.setColor(Color.BLACK);
         g.drawRect(300, 50, 200, 470);
+
+
     }
 
     public static void main(String[] args) {
--- tarea3/Problema103.java
+++ tarea3/Problema103.java
@@ -1,5 +1,6 @@
 package tarea3;
 import javax.swing.*;
+
 import interfaz.PanelPrincipal;
 
 
@@ -7,17 +8,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Problema103 {
+/*public class Problema103 {
     public static void main(String[] args) {
 
-        PanelPrincipal panel = new PanelPrincipal();
-        JFrame frame = new JFrame("a");
+        JFrame frame = new JFrame("PanelExpendedor");
+PanelPrincipal p1=new PanelPrincipal();
 
-        frame.add(panel);
-        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setSize(300, 300);
+        frame.setContentPane(p1);
 
+        frame.setVisible(true);
 
         /*
         Expendedor exp = new Expendedor(2);
@@ -27,7 +28,6 @@ public class Problema103 {
         c = new Comprador(m, 2, exp);
         System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
          */
-    }
+   /* }
 }
-
-
+*/
